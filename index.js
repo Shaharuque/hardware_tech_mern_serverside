5ac9386eb4f201dbdab3183bcad8da0b0732d6be
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-05-22 17:40:40 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log("Listening to server");
+});
