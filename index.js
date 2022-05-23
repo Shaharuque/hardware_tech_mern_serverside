f5f9d97de8815145a3eceef726ab80fc10fc4dc3
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-05-23 20:40:54 +0600

    mongo connect

--- index.js
+++ index.js
@@ -3,11 +3,36 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const res = require("express/lib/response");
 
 app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
+//mongoConnect
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cdxfa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("sea_tech").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("hellow");
