6546ecf6155611ce84b4a38527ccc225aa3bb9d1
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-05-24 23:45:26 +0600

    jwt verify admin

--- index.js
+++ index.js
@@ -19,6 +19,34 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Token" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+const verifyAdmin = async (req, res, next) => {
+  const requester = req.decoded.email;
+
+  const requesterAccount = await userCollection.findOne({
+    email: requester,
+  });
+
+  if (requesterAccount.role === "admin") {
+    next();
+  } else {
+    res.status(403).send({ message: "Forbidden" });
+  }
+};
 
 async function run() {
   try {
